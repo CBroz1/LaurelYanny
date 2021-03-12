1432cb93b27720a39c4dd8f6d42b378094cfa182
CBroz1 <CBrozdowski@yahoo.com>
2021-03-12 00:34:39 -0600

    Added Plot and Locations.txt

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ Publishing data collected based on the bistable auditory illusion.
 
 ## About
 
-In May 2018, a small clip went viral as an auditory equivalent of the blue or gold dress. An auditory bistable illusion that sounded like "Laurel" to some and "Yanny" to others. Wired has since published a [video discussing the topic](https://www.youtube.com/watch?v=3km896XZ-J0).
+In May 2018, a small clip went viral as an auditory equivalent of the blue or gold dress. An auditory bistable illusion that sounded like "Laurel" to some and "Yanney" to others. Wired has since published a [video discussing the topic](https://www.youtube.com/watch?v=3km896XZ-J0).
 
 I recognized that an individual's perception would likely shift with with pitch shifts. I used ffmpeg to generate shifted versions of the original clip, and concatenated them together in rising and falling sequences, shown [here](https://youtu.be/oaMTXfAZzpE). The original video accumulated about 15k views before I delisted it, and reposted to my personal YouTube. Some commenters requested an even more extreme shift, as shown [here](https://youtu.be/Nu4Ax459hoU).
-
-I was interested in when listeners percieved the inflection point between the to words, and whether or not this was linked to demographic informaiton, like age and gender. I posted a [Google form](https://docs.google.com/forms/d/e/1FAIpQLSczFWvoVw_nSRrVZVHnJxplkTFGHJnICps6NLE3z3iz-Cp-NA/viewform?usp=sf_link) to collect this data. A year later, it has 2600 responses, and one or two still come in each month. 
 
 ## EditVideo.sh
 
@@ -17,24 +15,51 @@ This script was made after the fact, with intent to publish to github. After dow
 2. Generates pitch shifted versions across a range of kHZ, and labels each
 3. Concatenates the new clips into a whole video of: original, rising pitches, original, falling pitches, and again the original.
 
+## Google form
+
+I was interested in when listeners perceived the inflection point between the to words, and whether or not this was linked to demographic information, like age and gender. I posted a [Google form](https://docs.google.com/forms/d/e/1FAIpQLSczFWvoVw_nSRrVZVHnJxplkTFGHJnICps6NLE3z3iz-Cp-NA/viewform?usp=sf_link) to collect this data. It has since 2600+ responses, and one or two still come in each month. Fast implementation of a form taught me some lessons.
+
+1. Google forms, in 2018, did not deal with integer inputs well. Age was therefore collected as a nominal rather than continuous variable.
+2. Survey respondents are primarily younger males.
+3. Free response location has many issues: 
+	* inconsistent *no-response* items (e.g., na vs. n/a)
+	* variable strings for the same location
+	* variable level of specificity (e.g., USA vs. California vs. San Diego)
+3. The absence of a 'feedback' free response resulted in some users providing comments when location was requested as free response. 
+
+If I had anticipated such a global response base, I would have worded this question differently, asking for country and separately asking for city, as well as prompting the user to skip the question if preferring not to respond.
+
+
 ## R Script
 
 The R script is divided into sections for:
 1) *Packages:* installing and loading relevant packages into R.
-2) *Data:* Accessing and cleaning up the raw data file. Saves processed file
+2) *Cleaning:* Accessing and cleaning up the raw data file. Saves processed file. More could be done for the free response data. The cleaned data has the following columns
+	* ID: Row ID
+	* Orig: Original perception 
+	* NumAv: Average inflection point
+	* NumUp: Ascending inflection point
+	* NumDn: Descending inflection point
+	* ChangeUp: Does it change going up?
+	* ChangeDn: Does it change going down?
+	* ChangeBt: Does it change either way?
+	* Gender: M, F, and NB for Gender-fluid/Non-binary/Other
+	* Age: High end of binned ranges from ordinal question. '65' for >60.
+	* LocUS: Nominal region within US. 
 3) *Stats:* Statistical tests.
-4) *Graphs:* Generating plots with ggplot
-5) *SPSS Code:* legacy SPSS code
+4) *SPSS Code:* legacy SPSS code
 
 ## Results
 
-1. Women are more likely to hear Yanny.
-2. Interpretations are preserved with pitch changes. Inflection points are higher when going up and lower coming down.
-3. Women who hear Larel in the original do not interpretation-preserve in the same way as others.
-4. Age is not a significant predictor, but trends indicate greater perception of Laurel with age.
+My first pass at the data with SPSS can be found [here](https://imgur.com/a/IClnLh4). The conclusions are similar, but the relationship between original interpretation and directional inflection points no longer differs by gender.
 
-## Drawbacks
+1. Women are more likely to hear Yanney.
+2. Interpretations are preserved with pitch changes. Inflection points are higher when going up and lower coming down. 
+3. Splitting results by original interpretation and gender.
+	* Women have higher inflection points than men.
+4. Age, when treated as continuous, is a significant predictor of
+	* Original perception. Greater perception of Laurel with age, but trend is driven by relatively few older respondents.
+	* Inflection point. Youngest and oldest respondents have highest and lowest inflection points respectively. 
 
-1. Google forms, in 2018, did not deal with integer inputs well. Age was therefore collected as a nominal rather than continuous variable.
-2. Survey respondents are primarily younger males.
+
 
--- _LY_Draft.R
+++ _LY_Draft.R
@@ -1,55 +1,119 @@
 
 # Packages ----------------------------------------------------------------
 install.packages('reshape2');install.packages('plyr');install.packages('dplyr')
-install.packages('tidyverse');install.packages('ggpubr');install.packages('rstatix');install.packages('datarium')
-library(reshape2);library(plyr);library(dplyr)
+install.packages('tidyverse');install.packages('ggpubr');install.packages('data.table')
+install.packages('rstatix');install.packages('datarium')
+library(reshape2);library(plyr);library(dplyr);library(data.table)
 library(tidyverse);library(ggpubr);library(rstatix);library(datarium)
-# Data --------------------------------------------------------------------
+# Cleaning ----------------------------------------------------------------
   setwd("{Redacted}/LaurelYanny")
   load("{Redacted}/LaurelYanny/.RData")
-  char_data <- read.csv('LaurelYannyData_Raw.csv', stringsAsFactors = F)
-  num_data <- data.frame(data.matrix(char_data))
-  numeric_columns <- sapply(num_data,function(x){mean(as.numeric(is.na(x)))<0.5})
-  formatted_data <- data.frame(num_data[,numeric_columns], char_data[,!numeric_columns])
-  df0 <- formatted_data
+  df0 <- read.csv('LaurelYannyData_Raw.csv')
   cat(colnames(df0), sep="\n")
-  names(df0)=c("NumUp", "NumDn", "TStamp", "Orig", "ChangeUp", "ChangeDn",
+  names(df0)=c("TStamp","Orig","ChangeUp","NumUp","ChangeDn","NumDn",
                "Gender", "Age", "Loc", "LocUS")
+  #Change Y/N to binary
   df0$ChangeUp <- ifelse(df0$ChangeUp=='Yes',1,0)
   df0$ChangeDn <- ifelse(df0$ChangeDn=='Yes',1,0)
+  #Set end of ranges to NA. Can't 'change' going up @0
   df0$NumUp <- ifelse(df0$ChangeUp==0,NA,df0$NumUp)
   df0$NumDn <- ifelse(df0$ChangeDn==0,NA,df0$NumDn)
   df0$NumUp <- ifelse(df0$NumUp==30,NA,df0$NumUp)
-  df0$Age <- mapvalues(df0$Age, from=c("<18", "18-25", "25-30", "30-35", "35-40",
-                                         "40-45", "45-50", "50-55", "55-60", ">60"),
-                        to=c(18, 25, 30, 35, 40, 45, 50, 55, 60, 65))
-  df0$Gender<-mapvalues(df0$Gender,from=c("Male","Female"),to=c("M","F"))
+  #Set age to 'max' of range
+  df0$Age <- mapvalues(df0$Age, from=c( "<18", "18-25", "25-30", "30-35", 
+                                        "35-40", "40-45", "45-50", "50-55", "55-60",
+                                        ">60","","Prefer not to say"),
+                        to=c(18, 25, 30, 35, 40, 45, 50, 55, 60, 65,NA,NA))
+  df0$Gender<-mapvalues(df0$Gender,from=c("Male","Female","Gender-fluid/Non-binary/Other",""),
+                        to=c("M","F","NB",NA))
   df0$NumAv<-rowMeans(df0[,c("NumDn", "NumUp")], na.rm=FALSE)
-  Locs <- unique(df0$Loc)
-  #dropping values with joke entries
-  DropLocs <- c(".", "956", "Big", "Did you just categorise my gender with gender-fluid and non-binary?", "Earth", "gkjhg", "Huh?", "Lol", "Mars", "My mom", "sneep snop", "The land of klepto	", "Ur mom", "Ur mom gay", "x", "X", "Your Moms Vagina", "z")
-  df1 <- df0[!(df0$Loc %in% DropLocs),]
-  df1$ID <- sprintf("%04s",rownames(df1))
-  df1$ChangeBt <- as.integer(df1$ChangeUp & df1$ChangeDn)
-  df<-df1[c("ID","Orig", "NumAv", "NumUp", "NumDn", "ChangeUp", "ChangeDn", "ChangeBt", "Gender", "Age", "LocUS")]
+  df0$Loc<-tolower(df0$Loc)
+  ##Light cleaning of free entry location
+  {
+    ##This data is diverse. Some cities, some states, some countries.
+    ##Would need a lot of hand-cleaning to one level of specificity
+    
+    #dropping values with joke entries, assuming poor data quality
+    DropLocs <- c("gkjhg", "huh?", "lol", "mars", "my", "my mama", "my mom", 
+                  "nigger", "sd153", "sneep snop", "the internet", "the land of klepto",
+                  "ur mom gay", "ur mum", "your moms vagina", 
+                  "did you just categorise my gender with gender-fluid and non-binary?")
+    df0 <- df0[!(df0$Loc %in% DropLocs),]
+    DropLocs <- c("", ".", "956", "choose not to answer", "h", "no where (military)", 
+                  "prefer not to say", "secret", "somewhere in europe", "somewhere", 
+                  "somwhere", "south", "x", "z","big","gh","na","n/a","not telling")
+    df0[(df0$Loc %in% DropLocs),]$Loc<-NA
+    DropLocs <- c("california, us","california, usa","ca","cali","california",
+                  "california/arizona","californian","sd","san diego, ca, usa",
+                  "san jose , ca","san jose, ca")
+    df0[(df0$Loc %in% DropLocs),]$Loc<-"CA"
+    DropLocs <- c("us","us - northeast originally", "usa", "usa.", "america",
+                  "america!!!!!!", "us- there wasn't another place for input so i'll say it here: i originally hear laurel. the questions for the speed-up seem to assume that i'll continue to hear laurel until a certain number is hit, but for me as i heard yanny from 30-88 and laurel for 40+. it was the same as when i heard it in the opposite direction.")
+    df0[(df0$Loc %in% DropLocs),]$Loc<-"USA"
+    fwrite(as.list(sort(unique(df0$Loc))), file = "Locations_cleaner.txt")
+  }
+  df0$ID <- sprintf("%04s",rownames(df0))
+  df0$ChangeBt <- as.integer(df0$ChangeUp & df0$ChangeDn) # Does it change for either?
+  df<-df0[c("ID","Orig", "NumAv", "NumUp", "NumDn", "ChangeUp", "ChangeDn", "ChangeBt", "Gender", "Age", "LocUS")]
   write.csv(df,file="LaurelYannyData_Clean.csv",na="")
+  #workspace cleanup 
   rm(list=setdiff(ls(), "df"))
   
 # Stats -------------------------------------------------------------------
-  df %>% group_by(Gender) %>%
-    summarise(n = n()) %>%
-    mutate(freq = n / sum(n))
+  # Treating initial interpretation as binary
+  df$Orig_bin <- as.numeric(mapvalues(df$Orig, from=c( "Laurel","Yanney"),to=c(1,0)))
   
-  binary <- c("M", "F")
-  dfb <- df[!(df$Gender %in% binary),] #only M and F
-
-# Graphs ------------------------------------------------------------------
-
-  st.err <- function(x) {sd(x)/sqrt(length(x))}
+  # ~ 5% is NB or no response, only keeping M/F for 'dfb' binary
+  df %>% group_by(Gender) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
+  binary <- c("M", "F"); dfb <- df[(df$Gender %in% binary),] #only M and F
+  rm(binary)
   
-  hist(df$NumDn, col=rgb(0,0,1,0.5),breaks=18,add=F)
-  hist(df$NumUp, col=rgb(1,0,0,0.5),breaks=18,add=T)
-  box()
+# Result 1 - Women are more likely to hear Yanney
+  t.test(Orig_bin ~ Gender, data=dfb)
+# Result 2 - Interpretations are preserved with pitch changes. 
+#            Women have higher inflection points
+#            Inflection points are higher when going up and lower coming down.
+  #transform to long
+  dfl<-(dfb %>% gather(key="Dir",value = "Pitch",NumUp,NumDn) %>% convert_as_factor(ID,Dir))
+  # drop NA values, restricting to only for whom changed
+  dfl<-dfl[!is.na(dfl$Pitch),]; df$Gender<- as.factor(df$Gender)
+  #ordinal data not normally distributed, esp for Male respondents. 
+  #not a great comparison given unbalanced N across cells
+  ggqqplot(dfl,"Pitch")+facet_grid(Gender~Dir,labeller="label_both")
+  res1.aov <- anova_test(data = dfl,dv=Pitch,wid=ID,
+                         within = c(Dir),between = c(Gender))
+  get_anova_table(res1.aov) # main effects, no interaction.
+  ggboxplot(dfl,x="Dir",y="Pitch",color="Gender")
+  t.test(Pitch ~ Dir, data=dfl) # Sig higher for NumUp
+  t.test(Pitch ~ Gender, data=dfl) # Sig higher for Female
+  
+# Result 3 - Those who initially hear Yanney have much higher inflection points.
+#            The distance between ascending and decending inflection points in
+#            smaller for those who initally year Laurel.
+  dfl<-dfl[!is.na(dfl$Orig_bin),] #For those who heard one the other initially
+  res2.aov <- anova_test(data = dfl,dv=Pitch,wid=ID,
+                         within = c(Dir),between = c(Gender,Orig_bin))
+  get_anova_table(res2.aov) # main effects, Orig*Dir interaction
+  t.test(Pitch ~ Dir, data=dfl) # Sig higher for NumUp
+  t.test(Pitch ~ Gender, data=dfl) # Sig higher for Female
+  
+  dfl$Gender <- factor(dfl$Gender, levels=c("M", "F"), labels=c("Male", "Female"))
+  dfl$Dir <- factor(dfl$Dir, levels=c("NumUp", "NumDn"), labels=c("Ascending", "Descending"))  
+  
+  ggboxplot(dfl,x="Gender",y="Pitch",color="Dir",facet.by="Orig")
+  
+
+# Result 4 - Age is not a significant predictor, but trends indicate greater 
+#            perception of Laurel with age.
+  # Treating age bin peak as continuous, despite ordinal rating.
+  df$Age <- as.numeric(df$Age)
+  cor.test(df$Age,df$Orig_bin)
+  df %>% group_by(Age) %>% summarize(average=mean(Orig_bin))
+  cor.test(df$Age,df$NumAv)
+  df[!is.na(df$NumAv),] %>% group_by(Age) %>% summarize(average=mean(NumAv))
+  
+
+
   
 # SPSS Code -----------------------------------------------------------------
   # * Encoding: UTF-8.
